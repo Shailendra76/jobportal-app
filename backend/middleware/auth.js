0103d872af12f92556c5b92b0cbbd5ea4ac8ed6a
shailendra76 <vsk60419@gmail.com>
2024-08-22 03:46:48 +0530

    ...

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,8 +5,8 @@ const User = require("../models/userModels"); // Change user to User
 
 // is user authenticated
 exports.isAuthenticated = async (req, res, next) => {
-    const { token } = req.cookies;
-    
+    // const { token } = req.cookies;
+    const { token } = req.token;
     
    
 
