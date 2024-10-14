128a293eea67b7a32c3071f5668dedbaab84e149
shailendra76 <vsk60419@gmail.com>
2024-10-14 11:12:39 +0530

    redeploy

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ const jobRoute = require('./routes/jobsRoutes')
 const cors = require('cors');
 
 const corsOptions = {
-  origin: "https://newdemo-ruby.vercel.app", // Replace with your Vercel frontend URL
+  origin: "https://talenthive.vercel.app", // Replace with your Vercel frontend URL
   credentials: true, // Allow cookies and credentials to be sent
 };
 
